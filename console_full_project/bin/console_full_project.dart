d24f4166d1dc9785cd403d9b7b58295a534a7a5a
CLS11 <crystall.101600@gmail.com>
2024-09-14 01:05:07 +0530

    first commit

---
+++ console_full_project/bin/console_full_project.dart
@@ -0,0 +1,9 @@
+import 'package:console_full_project/src/calculate.dart';
+
+void main() {
+  print('Add: ${calculateAdd()}');
+  print('Subtract: ${calculateSubtract()}');
+  print('Multiply: ${calculateMultiply()}');
+  print('Divide: ${calculateDivide()}');
+}
+    
---
+++ console_full_project/lib/src/calculate.dart
@@ -0,0 +1,17 @@
+import 'package:calculator/calculator.dart';
+
+int calculateAdd(){
+  return add(50, 80);
+}
+
+int calculateSubtract(){
+  return subtract(500, 30);
+}
+
+int calculateMultiply(){
+  return multiply(90, 80);
+}
+
+double calculateDivide(){
+  return divide(28, 7); 
+}
