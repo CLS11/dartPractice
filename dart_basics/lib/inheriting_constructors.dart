ca86b9a3bb39d7951ba65198a2818c25beeeb5c9
CLS11 <crystall.101600@gmail.com>
2024-09-27 00:02:27 +0530

    Inheriting the constructors in dart.

---
+++ dart_basics/lib/inheriting_constructors.dart
@@ -0,0 +1,22 @@
+void main(List<String> args) {
+  final mom = Mom();
+  print(mom.role);
+  final dad = Dad();
+  print(dad.role);
+}
+
+enum Role { mom, dad, son, grandpa, grandma }
+
+class Person {
+  final Role role;
+
+  const Person({required this.role});
+}
+
+class Mom extends Person {
+  const Mom() : super(role: Role.mom);
+}
+
+class Dad extends Person {
+  const Dad() : super(role: Role.dad);
+}
