93cb343787234ef812c66af7d763f23e18800560
CLS11 <crystall.101600@gmail.com>
2024-09-26 23:54:41 +0530

    Implementing a factory constructor.

---
+++ dart_basics/lib/factory_constructors.dart
@@ -0,0 +1,24 @@
+//Factory constructors can create instances of subclasses.
+
+void main(List<String> args){
+ print(Vehicle.car());
+ print(Vehicle.truck());
+}
+
+class Vehicle{
+  const Vehicle();
+
+  factory Vehicle.car() => Car();
+  factory Vehicle.truck() => Truck();
+
+  @override
+ String toString() => 'Vehicle of type $runtimeType';
+}
+
+class Car extends Vehicle{
+  const Car();
+}
+
+class Truck extends Vehicle{
+  const Truck();
+}
