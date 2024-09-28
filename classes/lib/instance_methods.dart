b92c27a1ca9257ff38c087c2a0ac31bb94d53ae2
CLS11 <crystall.101600@gmail.com>
2024-09-28 22:45:11 +0530

    Implementing a method.

---
+++ classes/lib/instance_methods.dart
@@ -0,0 +1,22 @@
+//Insance methods serve a specific purpose in the class.
+
+import 'dart:math';
+
+void main() {}
+
+class Point {
+  const Point({required this.x, required this.y});
+
+  num distanceTo(Point p) {
+    var dx = x - p.x;
+    var dy = y - p.y;
+    return sqrt(pow(dx, 2) + pow(dy, 2));
+  }
+
+  final int x;
+  final int y;
+  static const Point origin = Point(x: 0, y: 0);
+
+  @override
+  String toString() => 'Point(x: $x, y: $y)';
+}
